a8e5568736a09a1fb300581783b647e0a50407c4
denissalem <denissalem@tuxfamily.org>
2014-09-29 14:12:38 +0200

    progression on newEntry.java

--- src/venc/Core.java
+++ src/venc/Core.java
@@ -8,6 +8,7 @@ package venc;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import venc.i18n.I18nManager;
@@ -27,7 +28,62 @@ public class Core {
         DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         System.out.println(dateFormat.format(new Date())+" "+string);
     }
+    public static boolean isInteger(String str) {
+        int length = str.length();
+	if (str == null) {
+		return false;
+	}
+
+	if (length == 0) {
+		return false;
+	}
         
+	for (int j = 0; j < length; j++) {
+            char c = str.charAt(j);
+            if (c <= '/' || c >= ':') {
+                return false;
+            }
+	}
+	return true;
+    }
+
+    public Boolean isFileAnEntry(String fileName) {
+        String[] partedFileName = fileName.split("__");
+        
+        if (partedFileName.length < 3) {
+            return false;
+        }
+        
+        if (!this.isInteger(partedFileName[0])) {
+            return false;
+        }
+        if (!this.isDateValid(partedFileName[1])) {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isDateValid(String date) 
+    {
+        try {
+            DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+            df.setLenient(false);
+            df.parse(date);
+            return true;
+        }
+        catch (ParseException e) {
+            return false;
+        }
+    }
+
+
+    public int getLatestEntryId() {
+
+    
+        File dir = new File("entries");
+    
+        return 0;
+    }
     public boolean mkdir(String folder) {
         File theDir = new File(folder);
         boolean mkdirTrace = false;
--- src/venc/NewEntry.java
+++ src/venc/NewEntry.java
@@ -1,8 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+/* This software is under GNU/GPL v3 license, see
+http://www.gnu.org/copyleft/gpl.html */
+
 package venc;
 
 /**
