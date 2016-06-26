e1bdf2aae47ad3201a87790705982dd693be6759
denissalem <denissalem@tuxfamily.org>
2016-06-26 23:39:10 +0200

    modifié :         src/VenC/core.py

--- src/VenC/core.py
+++ src/VenC/core.py
@@ -17,6 +17,35 @@ def GetMessages():
 
 Messages = GetMessages()
 
+class key:
+    def __init__(self, value, entry):
+        self.weight = 1
+        self.value = value
+        self.relatedTo = [entry]
+        self.childs = list()
+
+
+class theme:
+    def __init__(self):
+        self.header = str()
+        self.footer = str()
+        self.entry = str()
+        self.rssHeader = str()
+        self.rssFooter = str()
+        self.rssEntry = str()
+
+        try:
+            self.header = open(os.getcwd()+"/theme/chunk/header.html",'r').read()
+            self.footer = open(os.getcwd()+"/theme/chunk/footer.html",'r').read()
+            self.entry = open(os.getcwd()+"/theme/chunk/entry.html",'r').read()
+            self.rssHeader = open(os.getcwd()+"/theme/chunk/rssHeader.html",'r').read()
+            self.rssFooter = open(os.getcwd()+"/theme/chunk/rssFooter.html",'r').read()
+            self.rssEntry = open(os.getcwd()+"/theme/chunk/rssEntry.html",'r').read()
+
+        except FileNotFoundError as e:
+            print("VenC: "+VenC.core.Messages.fileNotFound.format(str(e.filename)))
+            exit()
+
 def GetConfigurationFile():
     try:
         return yaml.load(open(os.getcwd()+"/blog_configuration.yaml",'r').read())
@@ -94,51 +123,53 @@ def SetNewEntryMetadata(entryDate, entryName):
 def PrintVersion(argv):
     print("VenC 1.0.0")
 
-def GetEntriesPerKeys(entries, key):
-    entriesPerKeys = dict()
+
+def GetKeyByName(keys, name):
+    for key in keys:
+        if key.value == name:
+            return key
+    return None
+
+def GetEntriesPerKeys(entries, keyType):
+    entriesPerKeys = list()
     for entry in entries:
         stream = open(os.getcwd()+"/entries/"+entry,'r').read().split("---\n")[0]
         data = yaml.load(stream)
-        for tag in data[key].split(","):
+        for tag in data[keyType].split(","):
             try:
-                entriesPerKeys[tag.strip()].append(entry)
-            except KeyError as e:
-                entriesPerKeys[tag.strip()] = [entry]
+                selectedKey = GetKeyByName(entriesPerKeys, tag)
+                selectedKey.relatedTo.append(entry)
+                selectedKey.weight+=1
+            except:
+                entriesPerKeys.append(key(tag,entry))
 
     return entriesPerKeys
 
 def GetEntriesPerDates(entries):
-    entriesPerDates = dict()
+    entriesPerDates = list()
     for entry in entries:
         date = time.strftime(blogConfiguration["path"]["dates_directory_name"], time.strptime(entry.split("__")[1],"%m-%d-%Y-%M-%S"))
         try:
+            selectedKey = GetKeyByName(entriesPerDates, date)
             entriesPerDates[date].append(entry)
-        except KeyError as e:
-            entriesPerDates[date] = [entry]
+            selectedKey.relatedTo.append(entry)
+            selectedKey.weight+=1
+        except:
+            entriesPerDates.append(key(date,entry))
 
     return entriesPerDates
 
 def GetEntriesPerCategories(entries):
-    entriesPerCategories = dict()
+    entriesPerCategories = list()
+    for entry in entries:
+        stream = open(os.getcwd()+"/entries/"+entry,'r').read().split("---\n")[0]
+        data = yaml.load(stream)
+        for category in data["categories"].split(","):
+            for subCategory in category.split(" > "):
+                try:
+                except:
+
+        
+        
         
 
-class theme:
-    def __init__(self):
-        self.header = str()
-        self.footer = str()
-        self.entry = str()
-        self.rssHeader = str()
-        self.rssFooter = str()
-        self.rssEntry = str()
-
-        try:
-            self.header = open(os.getcwd()+"/theme/chunk/header.html",'r').read()
-            self.footer = open(os.getcwd()+"/theme/chunk/footer.html",'r').read()
-            self.entry = open(os.getcwd()+"/theme/chunk/entry.html",'r').read()
-            self.rssHeader = open(os.getcwd()+"/theme/chunk/rssHeader.html",'r').read()
-            self.rssFooter = open(os.getcwd()+"/theme/chunk/rssFooter.html",'r').read()
-            self.rssEntry = open(os.getcwd()+"/theme/chunk/rssEntry.html",'r').read()
-
-        except FileNotFoundError as e:
-            print("VenC: "+VenC.core.Messages.fileNotFound.format(str(e.filename)))
-            exit()
