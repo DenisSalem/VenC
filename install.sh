c50b7d2edf79c3b3107ec1ce8cb686f27b9a3ec0
denissalem <denissalem@tuxfamily.org>
2016-08-10 10:15:20 +0200

    modifié :         install.sh 	modifié :         releases/1.0.0/VenC/export.py 	modifié :         releases/1.0.0/VenC/languages/fr.py 	modifié :         releases/1.0.0/VenC/new.py 	modifié :         releases/1.0.0/VenC/pattern.py 	modifié :         src/VenC/export.py 	modifié :         src/VenC/languages/fr.py 	modifié :         src/VenC/new.py 	modifié :         src/VenC/pattern.py

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+python=$(python -c "import sys; v=sys.version_info; print('python'+str(v[0])+'.'+str(v[1]))")
+version="1.0.0"
+
+cp -v   "releases/$version/venc.py"     "/usr/bin/venc"
+cp -vR  "releases/$version/VenC"        "/usr/lib/$python/"
+cp -vR  "releases/$version/share"  "/usr/share/VenC"
