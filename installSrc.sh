623ec7d0dc8f0bdb2e164408b6668fa858dce993
denissalem <denissalem@tuxfamily.org>
2016-08-22 10:56:44 +0200

    nouveau fichier : installSrc.sh

---
+++ installSrc.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+python=$(python -c "import sys; v=sys.version_info; print('python'+str(v[0])+'.'+str(v[1]))")
+version="1.0.0"
+
+cp -v   src/venc.py     /usr/bin/venc
+cp -vR  src/VenC        /usr/lib/$python/
+cp -vR  src/share       /usr/share/VenC
+chmod +rx     /usr/lib/$python/VenC
+chmod +rx -R  /usr/share/VenC
+chmod +rx     /usr/lib/$python/VenC/languages
+chmod +rx     /usr/lib/$python/VenC/*.py
+chmod +rx     /usr/lib/$python/VenC/languages/*.py
