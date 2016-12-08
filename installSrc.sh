823189ce4a0bd784f5856b26dcd6b9184d58e124
DenisSalem <denissalem@tuxfamily.org>
2016-12-08 13:48:11 +0100

    modifié :         ../../../../../installSrc.sh

--- installSrc.sh
+++ installSrc.sh
@@ -5,8 +5,6 @@ version="1.0.0"
 
 echo "Current $python"
 
-python="python3.5"
-
 cp -v   src/venc.py     /usr/bin/venc
 cp -vR  src/VenC        /usr/lib/$python/
 cp -vR  src/share/*     /usr/share/VenC
