ecad8cbb11f7266050c1221b56ba8a73640f316e
Denis Salem <denissalem@tuxfamily.org>
2017-02-11 00:20:08 +0100

    Add support

--- src/setup.py
+++ src/setup.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='VenC',
-    version='1.2.0',
-    description='A static blog generator',
+    version='1.2.0-2',
+    description='A static blog generator.',
     author='Denis Salem',
     author_email='denissalem@tuxfamily.org',
     url='https://github.com/DenisSalem/VenC',
     packages=['VenC', 'VenC.languages'],
+    license="GNU/GPLv3",
+    platforms="Linux",
+    long_description="A static blog generator, light and easy to use.",
     classifiers=[
         "Environment :: Console",
         "Development Status :: 5 - Production/Stable"
-    ]
+    ],
+    install_requires=[
+          'markdown',
+          'pyyaml',
+          'Pygments'
+    ],
+    scripts=['venc','venc-unit-test']
 )
