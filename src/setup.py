9e5895b051561996719d34ddfa2c78bf96f7fa6b
denissalem <denissalem@tuxfamily.org>
2018-09-13 15:30:30 +0200

    fix missing pep8, clean install, fix locale

--- src/setup.py
+++ src/setup.py
@@ -20,6 +20,20 @@
 from setuptools import setup
 import os
 
+dst_themes_path = os.path.expanduser('~')+"/.local/share/VenC/themes/"
+src_themes_path = "share/themes/"
+themes = os.listdir(src_themes_path)
+
+extra_files = []
+for theme in themes:
+    for sub_folder in os.listdir(src_themes_path+theme+'/'):
+        dst = dst_themes_path+theme+'/'+sub_folder
+        src_files = [src_themes_path+theme+'/'+sub_folder+'/'+f for f in os.listdir(src_themes_path+theme+'/'+sub_folder)]
+        extra_files.append((
+            dst,
+            src_files
+        ))
+
 setup(name='VenC',
     version='2.0.0',
     description='A static blog generator.',
@@ -47,70 +61,5 @@ setup(name='VenC',
           'Pygments'
     ],
     scripts=['venc','venc-unit-test','venc-blog-migration'],
-    data_files=[
-        
-        ### DUMMY
-
-        (
-            os.path.expanduser("~")+'/.local/share/VenC/themes/dummy/chunks',
-            [
-                'share/themes/dummy/chunks/header.html',
-                'share/themes/dummy/chunks/entry.html',
-                'share/themes/dummy/chunks/footer.html',
-                'share/themes/dummy/chunks/rssHeader.html',
-                'share/themes/dummy/chunks/rssEntry.html',
-                'share/themes/dummy/chunks/rssFooter.html'
-            ]
-        ),
-        (
-            os.path.expanduser("~")+'/.local/share/VenC/themes/dummy/assets',
-            [
-                'share/themes/dummy/assets/VenC-Infinite-Scroll-1.0.0.js'
-            ]
-        ),
-        
-        ### TESSELLATION
-
-        (
-            os.path.expanduser("~")+'/.local/share/VenC/themes/tessellation/chunks',
-            [
-                'share/themes/tessellation/chunks/header.html',
-                'share/themes/tessellation/chunks/entry.html',
-                'share/themes/tessellation/chunks/footer.html',
-                'share/themes/tessellation/chunks/rssHeader.html',
-                'share/themes/tessellation/chunks/rssEntry.html',
-                'share/themes/tessellation/chunks/rssFooter.html'
-            ]
-        ),
-        (
-            os.path.expanduser("~")+'/.local/share/VenC/themes/tessellation/assets',
-            [
-                'share/themes/tessellation/assets/VenC-Infinite-Scroll-1.0.0.js',
-                'share/themes/tessellation/assets/style.css',
-                'share/themes/tessellation/assets/styleEntry.css',
-                'share/themes/tessellation/assets/styleThread.css'
-            ]
-        ),
-        
-        ### GENTLE
-
-        (
-            os.path.expanduser("~")+'/.local/share/VenC/themes/gentle/chunks',
-            [
-                'share/themes/gentle/chunks/header.html',
-                'share/themes/gentle/chunks/entry.html',
-                'share/themes/gentle/chunks/footer.html',
-                'share/themes/gentle/chunks/rssHeader.html',
-                'share/themes/gentle/chunks/rssEntry.html',
-                'share/themes/gentle/chunks/rssFooter.html'
-            ]
-        ),
-        (
-            os.path.expanduser("~")+'/.local/share/VenC/themes/gentle/assets',
-            [
-                'share/themes/gentle/assets/VenC-Infinite-Scroll-1.0.0.js',
-                'share/themes/gentle/assets/style.css'
-            ]
-        )
-    ]
+    data_files = extra_files
 )
--- src/venc2/l10n/__init__.py
+++ src/venc2/l10n/__init__.py
@@ -1,26 +1,33 @@
 #! /usr/bin/python3
 
-#   Copyright 2016, 2017 Denis Salem
-
+#   Copyright 2016, 2018 Denis Salem
+#
 #    This file is part of VenC.
 #
 #    VenC is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
-
+#
 #    VenC is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
-
+#
 #    You should have received a copy of the GNU General Public License
 #    along with VenC.  If not, see <http://www.gnu.org/licenses/>.
 
 import locale
 
-currentLocale = locale.getlocale()[0].split('_')[0]
-if currentLocale == 'fr':
+try:
+    current_locale = locale.getlocale()[0].split('_')[0]
+    locale_err = False
+
+except:
+    current_locale = 'en'
+    locale_err = True
+
+if current_locale == 'fr':
     from venc2.l10n import fr as language
 
 else:
