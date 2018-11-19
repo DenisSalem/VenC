38960ce31762c6af93316a31fb035dba2adec9c4
Denis Salem <denissalem@tuxfamily.org>
2018-11-19 21:43:49 +0100

    fix missing imports

--- src/venc2/commands/remote.py
+++ src/venc2/commands/remote.py
@@ -17,6 +17,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with VenC.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
 import ftplib
 import socket
 import getpass
