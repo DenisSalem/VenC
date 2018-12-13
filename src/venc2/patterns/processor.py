5de5d203a5ebe3f004d9772a82e0f029fcd6d461
Denis Salem <denissalem@tuxfamily.org>
2018-12-13 23:46:24 +0100

    done with massive optimisation, again. Built site with ~770 entries in 2s. For real. :D

--- src/venc2/patterns/processor.py
+++ src/venc2/patterns/processor.py
@@ -117,6 +117,7 @@ class ProcessedString():
         self.ressource = ressource
         self.keep_appart_from_markup_indexes = list()
         self.keep_appart_from_markup_inc = 0
+        self.bop, self.bcp = [], []
 
     def keep_appart_from_markup_indexes_append(paragraphe, new_chunk, escape):
         if escape:
@@ -127,20 +128,20 @@ class ProcessedString():
         self.keep_appart_from_markup_inc +=1
         return output
 
-    def process_markup_language(self, markup_language, source):
+    def process_markup_language(self, markup_language):
         try:
-            self.string = parse_markup_language(self.string, markup_language, source)
+            self.string = parse_markup_language(self.string, markup_language, self.ressource)
         
         # catch error from reStructuredText
         except SystemMessage as e:
             try:
                 line = int(str(e).split(':')[1])
                 msg = str(e).split(':')[2].strip()
-                handle_markup_language_error(source+": "+msg, line=line, string=string)
+                handle_markup_language_error(self.ressource+": "+msg, line=line, string=string)
 
             except Exception as ee: 
                 print(ee, len(string.split('\n')))
-                handle_markup_language_error(source+", "+str(e))
+                handle_markup_language_error(self.ressource+", "+str(e))
         
         for triplet in self.keep_appart_from_markup_indexes:
             index, paragraphe, new_chunk = triplet
@@ -154,85 +155,6 @@ class ProcessedString():
                     "VENC_TEMPORARY_REPLACEMENT_"+str(index),
                     new_chunk
                 )
-"""
-class PreProcessor0ld():
-    def __init__(self, string):
-        self.blacklist = list()
-        self.sub_strings = list()
-        self.patterns_index = list()
-        close_symbol_pos = list()
-        open_symbol_pos	 = list()
-        i		 = int()
-        
-        while i < len(string):
-            if string[i:i+2] == ".:":
-                open_symbol_pos.append(i)
-
-            elif string[i:i+2] == ":.":
-                close_symbol_pos.append(i)
-
-            # At some point, when we get the same opening and closing symbols, it means that we just reach the end of a pattern
-            # so we can do the real job.
-            if len(close_symbol_pos) == len(open_symbol_pos) and len(close_symbol_pos) != 0 and len(open_symbol_pos) != 0:
-                # Append non pattern substring
-                self.sub_strings.append(string[i-close_symbol_pos[-1]:open_symbol_pos[0]])
-
-                # Append pattern substring
-                self.sub_strings.append(string[open_symbol_pos[0]:close_symbol_pos[-1]+2])
-
-                # Push pattern substring index in queue
-                self.patterns_index.append(len(self.sub_strings)-1)
-                string = string[close_symbol_pos[-1]+2:]
-                open_symbol_pos = list()
-                close_symbol_pos = list()
-                i=0
-            
-            else:
-                i+=1
-
-        # append last substring
-        self.sub_strings.append(string)
-        
-        # To avoid markup language collision we split appart Code snippet from the rest of the entry content
-        self.keep_appart_from_markup_index = list()
-
-    def process_markup_language(self, markup_language, source):
-        string = ""
-        for index in range(0, len(self.sub_strings)):
-            if not index in self.keep_appart_from_markup_index:
-                string += self.sub_strings[index]
-            
-            else:
-                string += "VENC_TEMPORARY_REPLACEMENT_"+str(index)
-        try:
-            string = parse_markup_language(string, markup_language, source)
-        
-        # catch error from reStructuredText
-        except SystemMessage as e:
-            try:
-                line = int(str(e).split(':')[1])
-                msg = str(e).split(':')[2].strip()
-                handle_markup_language_error(source+": "+msg, line=line, string=string)
-
-            except Exception as ee: 
-                print(ee, len(string.split('\n')))
-                handle_markup_language_error(source+", "+str(e))
-        
-        for pair in self.keep_appart_from_markup_index:
-            index, paragraphe = pair
-            if paragraphe:
-                string = string.replace(
-                    "<p>VENC_TEMPORARY_REPLACEMENT_"+str(index)+"</p>",
-                    self.sub_strings[index]
-                )
-            else:
-                string = string.replace(
-                    "VENC_TEMPORARY_REPLACEMENT_"+str(index),
-                    self.sub_strings[index]
-                )
-
-        return string
-"""
 
 class Processor():
     def __init__(self):
@@ -240,7 +162,6 @@ class Processor():
         self.functions		    = dict()
         self.current_input_string   = str()
         self.ressource              = str()
-        self.errors                 = list()
 
         # Some patterns are contextual while others are constant in time.
         # To save time we wan't to perform two-pass analysis. In the first one
@@ -268,6 +189,7 @@ class Processor():
                     ",;"+pattern+";;"+";;".join(argv)+";,",
                     error_origin = [':'+pattern+':']
                 )
+                print("THERE")
             else:
                 output = self.handle_error(
                     messages.unknown_contextual.format(e),
@@ -314,21 +236,15 @@ class Processor():
     # Print out notification to user and replace erroneous pattern
     def handle_error(self, error, default_output, error_origin = list()):
         err = get_formatted_message(error, "RED")+"\n"
-            
         if self.ressource != str():
-            err += get_formatted_message(messages.in_ressource.format(self.ressource)+"\n", "RED")
+            err = messages.in_ressource.format(self.ressource)+'\n'+err
                 
-        if not err in venc2.helpers.errors:
-            venc2.helpers.errors.append(err)
-            # error_origin is a array of substring value to replace in faulty string.
-            if error_origin != list():
-                err+=(''.join(self.current_string).strip())
-                for origin in error_origin:
-                    err = highlight_value(err, origin)
+        if error_origin != list():
+            err+=(''.join(self.current_input_string).strip())
+            for origin in error_origin:
+                err = highlight_value(err, origin)
             
-            print(err+"\n")
-        
-        return default_output
+        die(err, "RED")
 
     def set_function(self, key, function):
         self.functions[key] = function
@@ -361,57 +277,15 @@ class Processor():
 
         return True
 
-    """
-    # Process queue
-    def old_process(self, input_pre_processed, ressource, escape=False):
-        self.ressource = ressource
-        pre_processed = deepcopy(input_pre_processed)
-        if len(pre_processed.patterns_index) == 0:
-            return pre_processed
-        
-        self.current_string = pre_processed.sub_strings
-        to_remove = list()
-        for index in pre_processed.patterns_index:
-            current_pattern = pre_processed.sub_strings[index][2:-2].split('::')[0]
-
-            if current_pattern in ["CodeHighlight", "Latex2MathML", "IncludeFile", "audio", "video","EmbedContent"]:
-                pre_processed.keep_appart_from_markup_index.append((index,True))
-            
-            elif current_pattern in ["SetColor"]:
-                pre_processed.keep_appart_from_markup_index.append((index,False))
-
-            # this is tested twice???
-            if current_pattern in self.forbidden:
-                self.handle_error(
-                    messages.pattern_is_forbidden_here.format(current_pattern),
-                    ",;"+current_pattern+";;"+";;".join(pre_processed.sub_strings[index][2:-2])+";,",
-                    error_origin = [current_pattern]
-                )
-
-            if (not current_pattern in self.blacklist) and (not current_pattern in self.forbidden) :
-               if current_pattern == "Escape":
-                    pre_processed.sub_strings[index] = ''.join(pre_processed.sub_strings[index][2:-2].split('::')[1:])
-                    to_remove.append(index)
-                    continue
-                    
-                pre_processed.sub_strings[index] = self.process(pre_processed.sub_strings[index], escape)
-                
-                # check if there is residual patterns
-                if self.if_not_present(pre_processed.sub_strings[index]):
-                    to_remove.append(index)
-
-        for index in to_remove:
-            pre_processed.patterns_index = remove_by_value(pre_processed.patterns_index, index)
-
-        return pre_processed
-    """
-
     def process(self, pre_processed, escape=False):
         op, cp, string, lo, lc = pre_processed.open_pattern_pos, pre_processed.close_pattern_pos, pre_processed.string, pre_processed.len_open_pattern_pos, pre_processed.len_close_pattern_pos
         if lo == 0 and lc == 0:
-            return pre_processed
+            op, cp = sorted(op+pre_processed.bop), sorted(cp+pre_processed.bcp)
+            pre_processed.bop, pre_processed.bcp = [], []
+            pre_processed.len_open_pattern_pos, pre_processed.len_close_pattern_pos, pre_processed.open_pattern_pos, pre_processed.close_pattern_pos, pre_processed.string = len(op), len(cp), op, cp, string
+            return
         
-
+        self.current_input_string = string
         self.ressource = pre_processed.ressource
         while lo:
             diff = 18446744073709551616
@@ -429,7 +303,6 @@ class Processor():
 
             fields = [field.strip() for field in string[vop+2:vcp].split("::") if field != '']
             current_pattern = fields[0]
-            
             if current_pattern in self.forbidden:
                 self.handle_error(
                     messages.pattern_is_forbidden_here.format(current_pattern),
@@ -438,7 +311,7 @@ class Processor():
                 )
 
             """ réintégrer l'exclusi0n de certain pattern, réintégrer Escape N0Escape """
-            if (not current_pattern in self.blacklist) and (not current_pattern in self.forbidden) :
+            if (not current_pattern in self.blacklist) and (not current_pattern in self.forbidden):
                 if current_pattern in ["CodeHighlight", "Latex2MathML", "IncludeFile", "audio", "video","EmbedContent"]:
                     new_chunk = pre_processed.keep_appart_from_markup_index_append(
                         True,
@@ -457,24 +330,27 @@ class Processor():
                     new_chunk = self.run_pattern(current_pattern, fields[1:])
                     if escape:
                         new_chunk = cgi_escape(new_string)
-
+                    
                 string = string[0:vop] + new_chunk + string[vcp+2:]
                         
                 offset = len(new_chunk) - (vcp + 2 - vop)
-                for k in range(1,lo):
-                    if k >= i+1:
-                        op[k] += offset
-
-                    cp[k] += offset
+                op = [ (v+offset if v > vop else v) for v in op]
+                cp = [ (v+offset if v > vcp else v) for v in cp]
+                pre_processed.bop = [ (v+offset if v > vop else v) for v in pre_processed.bop]
+                pre_processed.bcp = [ (v+offset if v > vcp else v) for v in pre_processed.bcp]
 
                 op.pop(i)
                 cp.pop(j)
-                lo -= 1
-                lc -= 1
-                continue
 
-        if len(self.blacklist):
-            pre_processed.len_open_pattern_pos, pre_processed.len_close_pattern_pos = lo, lc
-            return pre_processed
+            elif current_pattern == "GetRelativeOrigin":
+                op.pop(i)
+                cp.pop(j)
 
-        return self.process(pre_processed, escape)
+            else:
+                pre_processed.bop.append( op.pop(i) )
+                pre_processed.bcp.append( cp.pop(j) )
+
+            lo -= 1
+            lc -= 1
+        pre_processed.len_open_pattern_pos, pre_processed.len_close_pattern_pos, pre_processed.open_pattern_pos, pre_processed.close_pattern_pos, pre_processed.string = lo, lc, op, cp, string
+        self.process(pre_processed, escape)
