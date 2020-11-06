7cf4b1b4d2aaa4582af0b0c35989be5ffd786008
denissalem <denissalem@tuxfamily.org>
2020-11-06 15:55:49 +0100

    substantial improvement of VenC-Tree: hierarchical list automaticaly unfold to active chapter or category, code cleaning

--- VenC-site/extra/VenC-Tree-0.1.0.js
+++ VenC-site/extra/VenC-Tree-0.1.0.js
@@ -23,68 +23,133 @@ var VENC_TREE = {
     button_disabled: '○'
 }
 
-function VENC_TREE_ON_CLICK() {
-    ul = this.parentElement.children[2];
-    if (this.innerHTML == VENC_TREE.button_show) {
-        if (ul != undefined) {
-            ul.style.transition = "0.25s ease all";
-            ul.style.height = "auto";
-            ul.style.opacity = "1";
-            ul.style.padding = "5px";
-        }
-        for (i = 0; i < ul.children.length; i++) {
-            child = ul.children[i];
-            if (child.children[1] != undefined) {
-                if (child.children[0].className != "__VENC_TREE_BUTTON_SHOW__" && child.children[0].className != "__VENC_TREE_BUTTON_HIDE__" && child.children[0].className != "__VENC_TREE_BUTTON_DISABLED__") {
-                    child.innerHTML = "<a href=\"\" class=\"__VENC_TREE_BUTTON_SHOW__\">"+VENC_TREE.button_show+"</a>"+ child.innerHTML;
-                    child.children[0].onclick = VENC_TREE_ON_CLICK;
-                }
+function VENC_TREE_SWITCH_NODE_VISIBILIY(button) {
+    Array.from(button.parentNode.children).forEach(function(child, child_index, childs_array) {
+        if (child.className.includes("__VENC_TREE_NODE__")) {
+            if (button.innerHTML == VENC_TREE.button_hide) {
+                VENC_TREE_UNHIDE_ELEMENT(child)
             }
             else {
-                child.innerHTML = "<span class=\"__VENC_TREE_BUTTON_DISABLED__\">"+VENC_TREE.button_disabled+"</span>"+ child.innerHTML;
+                VENC_TREE_HIDE_ELEMENT(child)
             }
         }
-        this.innerHTML = VENC_TREE.button_hide;
+    })
+}
+
+function VENC_TREE_SWITCH_BUTTON(button) {
+    if (button.innerHTML == VENC_TREE.button_show) {
+        button.innerHTML = VENC_TREE.button_hide;
+        button.className = "__VENC_TREE_BUTTON_HIDE__"
     }
     else {
-        ul.style.height = "0px";
-        ul.style.overflow = "hidden";
-        ul.style.opacity = "0";
-        ul.style.padding = "0px";
-        this.innerHTML = VENC_TREE.button_show;
+        button.innerHTML = VENC_TREE.button_show;
+        button.className = "__VENC_TREE_BUTTON_SHOW__"
     }
-    
+}
+
+function VENC_TREE_SWITCH_STATE(button) {
+    VENC_TREE_SWITCH_BUTTON(button)
+    VENC_TREE_SWITCH_NODE_VISIBILIY(button)
+}
+
+function VENC_TREE_ON_CLICK() {
+    VENC_TREE_SWITCH_STATE(this)  
     return false;
 }
 
-function VENC_TREE_HIDE(nodes) {
-    for (k = 1; k < nodes.length; k++) {
-        nodes[k].style.height = "0px";
-        nodes[k].style.overflow = "hidden";
-        nodes[k].style.opacity = "0";
-        nodes[k].style.padding = "0px";
+function VENC_TREE_UNHIDE_ELEMENT(element) {
+    element.style.transition = "0.25s ease all";
+    element.style.height = "auto";
+    element.style.opacity = "1";
+    element.style.padding = "5px";
+    element.setAttribute("data-venc-state", "visible")
+}
+
+function VENC_TREE_HIDE_ELEMENT(element) {
+    element.style.height = "0px";
+    element.style.overflow = "hidden";
+    element.style.opacity = "0";
+    element.style.padding = "0px";
+    element.setAttribute("data-venc-state", "hidden")
+}
+
+function VENC_TREE_ADD_BUTTON(button) {
+    a = document.createElement("a")
+    a.href=''
+    if ( !(button === VENC_TREE.button_disabled) ) {
+        a.onclick = VENC_TREE_ON_CLICK
     }
+    a.innerHTML = button
+    switch(button) {
+        case VENC_TREE.button_show:
+            a.className = "__VENC_TREE_BUTTON_SHOW__"
+            break
+        case VENC_TREE.button_hide:
+            a.className = "__VENC_TREE_BUTTON_HIDE__"
+            break
+        default:
+            a.className = "__VENC_TREE_BUTTON_DISABLED__"
+    }
+    return a
+}
+
+function VENC_TREE_NODE_HAS(node, target) {
+    childs = node.getElementsByClassName("__VENC_TREE_PATH__")
+    for (i = 0; i < childs.length; i++) {
+        if (target == childs[i]) {
+            return true
+        }
+    }
+    return false
 }
 
 function VENC_TREE_ON_LOAD() {
-    roots = document.getElementsByClassName("__VENC_TREE_ROOT__");
-    console.log("Roots:",roots.length);
-    for (i = 0; i < roots.length; i++) {
-        console.log("Root index", i);
-        nodes = roots[i].getElementsByClassName("__VENC_TREE_NODE__");
-        console.log("Nodes:",nodes.length);
-        VENC_TREE_HIDE(nodes);
-        for (j = 0; j < nodes[0].children.length; j++) {
-            child = nodes[0].children[j];
-            if (child.children[1] != undefined) {
-                child.innerHTML = "<a href=\"\" class=\"__VENC_TREE_BUTTON_SHOW__\">"+VENC_TREE.button_show+"</a>"+ child.innerHTML;
-                child.children[0].onclick = VENC_TREE_ON_CLICK;
-            }
-            else {
-                child.innerHTML = "<span class=\"__VENC_TREE_BUTTON_DISABLED__\">"+VENC_TREE.button_disabled+"</span>"+child.innerHTML;
-            }
+    
+    // Unhide current branch
+    path_hrefs = document.getElementsByClassName("__VENC_TREE_PATH__");
+    path_href = undefined
+    for (i = 0; i < path_hrefs.length; i++) {
+        if (window.location.pathname.includes(path_hrefs[i].pathname)) {
+            path_href = path_hrefs[i];
+            break
         }
     }
+    
+    roots = Array.from(document.getElementsByClassName("__VENC_TREE_ROOT__"))
+    roots.forEach(function(root, root_index, roots_array) {
+        nodes = Array.from(root.getElementsByClassName("__VENC_TREE_NODE__"))
+        
+        // First Pass: Setup the whole tree, and making visible active sublist
+        nodes.forEach(function(node, node_index, nodes_array) {
+            node_has_path_href = VENC_TREE_NODE_HAS(node, path_href)
+            
+            if (node.parentNode != root && !node_has_path_href) { 
+                VENC_TREE_HIDE_ELEMENT(node)
+            }
+            else {
+                VENC_TREE_UNHIDE_ELEMENT(node)
+            }
+            
+            items = Array.from(node.children)
+            items.forEach(function(item, item_index, items_array) {
+                button = item.getElementsByClassName("__VENC_TREE_NODE__").length > 0 ? VENC_TREE.button_show : VENC_TREE.button_disabled
+                item.insertBefore(
+                    VENC_TREE_ADD_BUTTON(button),
+                    item.firstChild
+                )
+            })
+        })
+        
+        // Second Pass: Update button acordingly with sublist visibility
+        nodes.forEach(function(node, node_index, nodes_array) {
+            button = node.parentNode.firstChild
+            if (node.getAttribute("data-venc-state") === "visible") {
+                if( button.nodeType !== 3 && button.className.includes("__VENC_TREE_BUTTON_SHOW__")) {
+                    VENC_TREE_SWITCH_BUTTON(button)
+                }
+            }
+        })
+    })
 }
 
 VENC_ON_LOAD_CALLBACK_REGISTER.push(VENC_TREE_ON_LOAD);
