6797e3e8751858b2a91a2625f6edc030ae46e63b
Denis Salem <denissalem@tuxfamily.org>
2021-07-28 14:03:44 +0200

    Version 2.0.1, update setup.py

--- src/setup.py
+++ src/setup.py
@@ -87,7 +87,7 @@ extra_files.append(
 
 setup(
     name='VenC',
-    version='2.0.0',
+    version='2.0.1',
     description='A static blog generator.',
     author='Denis Salem',
     author_email='denissalem@tuxfamily.org',
