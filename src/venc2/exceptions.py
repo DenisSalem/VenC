18c1db5a5a7b8b2c9c1fdd8a2bfbdc961d632a20
dsalem <dsalem@groupeot.com>
2022-03-11 23:13:53 +0100

    Nicer malformed pattern exception handling

--- src/venc2/exceptions.py
+++ src/venc2/exceptions.py
@@ -22,6 +22,7 @@ from venc2.l10n import messages
 class VenCException(Exception):
     def __init__(self, message, context=None):
         self.message = message
+        self.extra = ""
         self.context = context
     
     def __str__(self):
@@ -32,7 +33,7 @@ class VenCException(Exception):
 
     def die(self):
         from venc2.prompt import die
-        die(self.message)
+        die(self.message, extra=self.extra)
         
 class MalformedPatterns(VenCException):
     def __init__(self, string_under_processing):
@@ -43,33 +44,42 @@ class MalformedPatterns(VenCException):
             m = messages.malformed_patterns_missing_closing_symbols.format(string_under_processing.context, len_op - len_cp)
         else:
             m = messages.malformed_patterns_missing_opening_symbols.format(string_under_processing.context, len_cp - len_op)
+            
+        leftovers = string_under_processing.op if too_many_opening_symbols else string_under_processing.cp
+        s = string_under_processing.string
+        for leftover in leftovers:
+            s = s[:leftover]+"\x00\x00"+s[leftover+2:]
+        
+        s = s.replace("\x00\x00", '\033[91m'+( ".:" if too_many_opening_symbols else ":.") +'\033[0m')
         
         super().__init__(m, string_under_processing.context)
         self.too_many_opening_symbols = too_many_opening_symbols
+        self.extra = s
+
         
 # Special case of KeyError
-class UnknownContextual(KeyError):
-    pass
+# ~ class UnknownContextual(KeyError):
+    # ~ pass
 
-class IllegalUseOfEscape(Exception):
-    def __init__(self, ressource):
-        self.ressource = ressource
-        pass
+# ~ class IllegalUseOfEscape(Exception):
+    # ~ def __init__(self, ressource):
+        # ~ self.ressource = ressource
+        # ~ pass
 
-class PatternInvalidArgument(Exception):
-    def __init__(self, name, value, message=''):
-        self.name = name
-        self.value = value
-        self.message = message
+# ~ class PatternInvalidArgument(Exception):
+    # ~ def __init__(self, name, value, message=''):
+        # ~ self.name = name
+        # ~ self.value = value
+        # ~ self.message = message
 
-class PatternMissingArguments(Exception):
-    def __init__(self, e=messages.not_enough_args, expected=1, got=0):
-        if type(e) != str:
-            v = e.args[0].replace(',',' ').replace(')',' ').split()
-            self.expected, self.got = tuple([int(s) for s in v if s.isdigit()])
-            self.info = messages.not_enough_args.format(self.expected, self.got)
+# ~ class PatternMissingArguments(Exception):
+    # ~ def __init__(self, e=messages.not_enough_args, expected=1, got=0):
+        # ~ if type(e) != str:
+            # ~ v = e.args[0].replace(',',' ').replace(')',' ').split()
+            # ~ self.expected, self.got = tuple([int(s) for s in v if s.isdigit()])
+            # ~ self.info = messages.not_enough_args.format(self.expected, self.got)
 
-        else:
-            self.expected = expected
-            self.got = got
-            self.info = e.format(expected,got)
+        # ~ else:
+            # ~ self.expected = expected
+            # ~ self.got = got
+            # ~ self.info = e.format(expected,got)
