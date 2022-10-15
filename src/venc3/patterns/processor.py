e16631c8c4a7d598e888b8db5290b75867598307
dsalem <dsalem@groupeot.com>
2022-10-15 18:23:10 +0200

    try another approach again ...

--- src/venc3/patterns/processor.py
+++ src/venc3/patterns/processor.py
@@ -22,221 +22,66 @@ from time import time
 
 from venc3.patterns.patterns_map import PatternsMap
 
-class Pattern:
-    def __init__(self, o, c):
-        self.o, self.c = o, c
-        self.patterns = []
-        # TO BE DEFINED LATER
-        # ~ self.payload = None
-        # ~ self.parent_arg_index = None
-        # ~ self.flags = PatternsMap.FLAG_NONE
-        
-    def __iter__(self):
-        return iter((self.o, self.c))
+def __find_pattern_boundaries(string, symbol):
+  index = 0
+  boundary_type = Boundary.BONDARY_TYPE_OPENING if symbol == ".:" else Boundary.BONDARY_TYPE_CLOSING
+  while '∞':
+      index = string.find(symbol, index)
+      if index == -1:
+          break
+          
+      yield Boundary(index, boundary_type)
+      index+=1
+
+
+class Boundary:
+    BONDARY_TYPE_OPENING = True
+    BONDARY_TYPE_CLOSING = False
     
-class VenCString:  
-    def __init__(self, string, context=None, shield=False):
-        self.o, self.c, self.payload = 0, 0, [""]
+    def __init__(self, index, boundary_type):
+        self.index = index
+        self.boundary_type = boundary_type
+        self.level = 0
         
-        self._str = string
-        self.context = context
-        self.patterns, escape_indexes = [], []
-        patterns_append, escape_indexes_append = self.patterns.append, escape_indexes.append
-        self.pattern_identifier = 0
-                
-        # This block get indexes of opening and closing patterns.
-        op = VenCString.__find_pattern_boundaries(string, '.:')
-        cp = VenCString.__find_pattern_boundaries(string, ':.')
-                
-        if len(op) != len(cp):
-            self.op, self.cp = op, cp
-            from venc3.exceptions import MalformedPatterns
-            raise MalformedPatterns(self)
-        
-        # Pairing
-        i, op_pop, cp_pop = 0, op.pop, cp.pop
-        while i < len(op):
-            o, c = op[i], cp[0]
-            if i < len(op)-1:
-                next_o = op[i+1]
-                if o < next_o < c :
-                    i+=1
-                    continue
+    def __repr__(self):
+        return ".:" if self.boundary_type == Boundary.BONDARY_TYPE_OPENING else ":."
 
-            o, c = op_pop(i), cp_pop(0)
-            if string[o:o+10] == ".:Escape::":    
-                escape_indexes_append((o,c+2))
-
-            patterns_append(Pattern(o, c+2))
-            i = 0
-            
-        escape_indexes.sort(key=lambda p:p[0], reverse=True)
-        self.patterns.sort(key=lambda p:p.o, reverse=True)
-        
-        # Strong bottleneck happen for pattern escaping. But we don't care since there is no real use case
-        # for regular user.
-
-        # Drop escaped ... escapes
-        i = 0
-        escape_indexes_pop = escape_indexes.pop
-        while i < len(escape_indexes) - 1:
-            eo, ec = escape_indexes[i]
-            if escape_indexes[i+1][1] > ec:
-                escape_indexes_pop(i)
-                
-            else:
-                i+=1
-
-        # Drop escaped patterns
-        for eo, ec in escape_indexes:
-            # Binary search
-            lo, hi = 0, len(self.patterns) - 1
-            while lo < hi:
-                mid = lo + (hi - lo) // 2
-                if self.patterns[mid].c <= eo:
-                    hi = mid
-                    
-                else:
-                    lo = mid + 1
-
-            # updating string
-            escaped = string[eo+10:ec-2].strip()
-            offset = (ec - eo) - len(escaped) 
-            self._str = self._str[:eo] + escaped + self._str[ec:]
-
-            self.patterns = [ pattern for pattern in self.patterns[:lo] if VenCString.__drop_escaped_pattern(eo, ec, pattern, offset)] + self.patterns[lo:]
-
-        # Make a tree by chunking group of patterns
-        final_set_of_patterns = []
-        while len(self.patterns):
-            current_patterns_block = [self.patterns.pop()]
-            while len(self.patterns):
-                # greater OR equal because adjacent pattern might have o == c
-                if self.patterns[-1].o >= current_patterns_block[0].c:
-                    break
-                    
-                current_patterns_block.append(self.patterns.pop())
-                
-            current_patterns_block = current_patterns_block[::-1]
-            
-            i, old_len_patterns = 0, len(current_patterns_block)
-            while i < len(current_patterns_block):
-                current_patterns_block = [ pattern for pattern in current_patterns_block if VenCString.__pattern_extraction(current_patterns_block[i], pattern) ]
-                if old_len_patterns == len(current_patterns_block):
-                    i+=1
-                  
-                else:
-                    diff = old_len_patterns - len(current_patterns_block)
-                    if i - diff >= 0:
-                        i -= diff
-                        
-                    old_len_patterns = len(current_patterns_block)
-                                 
-            final_set_of_patterns = current_patterns_block + final_set_of_patterns
-                    
-        self.patterns = final_set_of_patterns
-
-        self.__finalize_patterns_tree(self.patterns, self)
-
-    def __finalize_patterns_tree(self, patterns, parent):      
-        for pattern in patterns:
-            self.__finalize_patterns_tree(pattern.patterns, pattern)
-            
-            pattern.payload = self._str[pattern.o+2:pattern.c-2].split('::')
-
-            # ~ Assign sub_pattern a location in args, updatating index as well
-            if len(pattern.patterns):
-                payload = pattern.payload[1:]
-                for sub_pattern in pattern.patterns:
-                    # locate sub_pattern in args
-                    length = 0
-                    arg_index = 0
-                    for arg in payload:
-                        length += 2+len(arg)
-                        if length > sub_pattern.o:
-                            break
-                            
-                        arg_index += 1
-                        
-                    arg_offset = len('::'.join(payload[:arg_index])) + (2 if arg_index else 0)
-                    sub_pattern.o -= arg_offset 
-                    sub_pattern.c -= arg_offset
-                    sub_pattern.parent_arg_index = arg_index + 1
-                                
-            # Make pattern relative to parent's arg
-            offset = parent.o if parent == self else ( parent.o + 4 + self._str[parent.o:parent.c].index('::',0) - 2 )
-            pattern.o -= offset
-            pattern.c -= offset
-                    
-    @staticmethod
-    def __find_pattern_boundaries(string, symbol):
-      l = list()
-      l_append = l.append
-      index=0
-      while '∞':
-          index = string.find(symbol, index)
-          if index == -1:
-              return l
-          l_append(index)
-          index+=1
+def __get_boundaries(string):
+    o = [o for o in __find_pattern_boundaries(string, ".:")]
+    c = [c for c in __find_pattern_boundaries(string, ":.")]
     
-    @staticmethod
-    def __drop_escaped_pattern(eo, ec, pattern, offset):
-        if pattern.o >= eo and pattern.c <= ec:
-            return False
-                    
-        elif pattern.o >= ec: 
-            pattern.o -= offset
-            pattern.c -= offset
-            
-        return True
-        
-    @staticmethod
-    def __pattern_extraction(destination, target):
-        if target.o > destination.o and target.c < destination.c:
-            destination.patterns.append(target)
-            return False
-                        
-        return True
-
-class VenCProcessor:
-    def __init__(self):
-        self.functions = {}
-        self.set_patterns = self.functions.update
-        
-from math import log10
-count= 1
-step = 10**(log10(count)-1)
-
-t = time()
-for i in range(0,count):
-    if i % step == 0:
-        print(i, time() - t)
-        t = time()
-        
-    vs = VenCString(".:TEST:: .:DEEPER_TEST:. :. .:Escape_:: .:Escaped:. :. .:Escape_:: .:LEVEL1:: .:LEVEL2_BIS:: .:LEVEL3:. :. .:LEVEL2:: .:LEVEL3:: .:LEVEL4:. :. :: .:LEVEL3_BIS:. .:LEVEL3_BIS_LE_RETOUR:. :. :. :."*1, "test")
-    # ~ vs = VenCString(".:LEVEL1_BIS:. .:LEVEL1:: .:LEVEL2:. :. :: .:LEVEL2_BIS :: .:LEVEL2_BIS_BIS:: .:LEVEL3:. :. :. "*1, "test")
+    if len(o) != len(c):
+        from venc3.exceptions import VenCException
+        raise VenCException
     
-def print_tree(vs, nodes, indent=''):
-    for pattern in nodes.patterns:
-        print_tree(vs, pattern, indent+'\t')
-        
-        if nodes == vs:
-            print(indent, vs._str[pattern.o:pattern.c])
-            
+    return sorted( 
+        o + c,
+        key = lambda x: x.index
+    )
+
+def __get_boundaries_block(boundaries):
+    if boundaries[0].boundary_type == Boundary.BONDARY_TYPE_CLOSING:
+        from venc3.exceptions import VenCException
+        raise VenCException
+    
+    level = 0
+    for i in range(0, len(boundaries)):
+        if boundaries[i].boundary_type == Boundary.BONDARY_TYPE_CLOSING:
+          level -= 1
+          
         else:
-            print(indent, nodes.payload[pattern.parent_arg_index][pattern.o:pattern.c])
-            # ~ print(indent, '::'.join(nodes.payload[1:])[pattern.o:pattern.c])
+          level += 1
+          
+        if level == 0:
+            block = boundaries[:i+1]
+            del boundaries[:i+1]
+            return block
 
-
-print_tree(vs, vs)
-
-# ~ print()
-# ~ print(vs._str)
-# ~ print()
-
-# ~ i = 0
-# ~ for pattern in vs.patterns:
-    # ~ print(i, pattern.o, pattern.c, ">"+vs._str[pattern.o:pattern.c]+"<")
-    # ~ i+=1
-
+s = ".:LEVEL1:. .:LEVEL2:: .:LEVEL3:. :."*1000
+for i in range(0, 1000):
+    b = __get_boundaries(s)
+    blocks = []
+    while len(b):
+        blocks.append(
+            __get_boundaries_block(b)
+        )
