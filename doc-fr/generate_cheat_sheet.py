c1ea5b8452d6aedd462a58e22bf6d7b4ffe93e68
Denis Salem <denissalem@tuxfamily.org>
2023-05-08 12:41:18 +0200

    done with 4.5

--- doc-fr/generate_cheat_sheet.py
+++ doc-fr/generate_cheat_sheet.py
@@ -27,12 +27,14 @@ for index in source.keys():
             attr = getattr(importlib.import_module(module, package), function)
         else:
             attr = getattr(source[index][1], symbol)
+        variables = ("<ul>"+''.join(["<li>"+v+"</li>" for v in attr.__doc__.split(',')])+"</ul>") if attr.__doc__ != None else ""
         args = ', '.join([(name if name != "argv" else "arg 1</li><li>arg 2</li><li>arg n...") for name in inspect.signature(attr).parameters if not name in ["self","pattern","raise_error", "raise_exception"]])
         value = "<a href=\".:GetChapterAttributeByIndex::path::"+index+":.#"+pattern.lower()+"\">"+pattern+"</a>"
         patterns.append((
             pattern,
             value,
-            args
+            args,
+            variables
         ))
 
 print(".:Table::")
@@ -40,5 +42,5 @@ print("Motifs::Arguments::Variables::NewLine::")
 items = sorted(patterns, key = lambda x: x[0])
 for i in range(0, len(items)):
     pattern = items[i]
-    print(pattern[1]+"::<ul>"+''.join(["<li>"+v+"</li>" for v in pattern[2].split(",") if len(v)])+"</ul>:: "+("::NewLine::" if i < len(items)-1 else ""))
+    print(pattern[1]+"::<ul>"+''.join(["<li>"+v+"</li>" for v in pattern[2].split(",") if len(v)])+"</ul>::"+pattern[3]+("::NewLine::" if i < len(items)-1 else ""))
 print(":.")
