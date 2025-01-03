171adcb7b7c1503043702aafc0b9d603759ee870
dsalem <dsalem@groupeot.com>
2025-01-03 14:46:19 +0100

    fix framagit issue #106

--- src/venc3/commands/new.py
+++ src/venc3/commands/new.py
@@ -44,7 +44,7 @@ def new_entry(params):
     
     entry = dict()
     try:
-        entry["ID"] = max([ int(filename.split("__")[0]) for filename in yield_entries_content()]) + 1
+        entry["ID"] = max([ int(filename.split("/")[-1].split("__")[0]) for filename in yield_entries_content()]) + 1
 
     except ValueError:
         entry["ID"] = 1
