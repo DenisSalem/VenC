0b7fe83ab434a92beab0a9e00369c6b364c682b1
Denis Salem <denissalem@tuxfamily.org>
2026-06-11 21:39:58 +0200

    Add customizable placeholder for WebGL, update venc media viewer css

--- src/venc3/patterns/non_contextuals.py
+++ src/venc3/patterns/non_contextuals.py
@@ -130,7 +130,7 @@ def escape(pattern, *content):
     return escape_walk(pattern, pattern)
     
 def webgl(pattern, uri, width, height):
-    return "<canvas class=\"VENC_WEB_GL_CANVAS\" width=\"{0}\" height=\"{1}\" data-venc-webgl-mesh=\"{2}\" title=\"{2}\" alt=\"{2}\">{2}: canvas is not supported.</canvas>".format(
+    return "<canvas class=\"VENC_WEB_GL_CANVAS\" width=\"{0}\" height=\"{1}\" data-venc-webgl-mesh=\"{2}\" title=\"{2}\" alt=\"{2}\"><span class=\"VENC_CANVAS_UNAVAILABLE\">{2}: canvas is not supported.</span></canvas>".format(
         width,
         height,
         uri
